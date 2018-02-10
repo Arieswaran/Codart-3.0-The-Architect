e77e03a8dc139d76059caa6ff74644d18740c154
Arieswaran <arieswaran@gmail.com>
2018-02-10 17:01:41 +0530

    Not working

--- Ravi and fruits.py
+++ Ravi and fruits.py
@@ -2,7 +2,12 @@ n=int(input())
 for i in range(n):
     a=[int(j) for j in (raw_input().strip()).split(' ')]
     c=1
+    cc=2
+    t=1
     for j in range(1,a[0]):
-        c+=c+2
+        c+=cc
+        cc+=1
+        #t+=c
+    #print t,c
     r=c%a[1]
     print r
